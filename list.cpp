3d64c24a3f06bd2ce796901e626d71cd985fa783
Carlos R. Arias <arias@spu.edu>
2020-11-13 16:51:03 -0800

    Added list.cpp stubs

--- list.cpp
+++ list.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "list.h"
+
+List::List(const List &original) {
+
+}
+
+const List &List::operator=(const List &rhs) {
+	return *this;
+}
+
+List::List() {
+
+}
+
+List::~List() {
+
+}
+
+bool List::Insert(const Book &book, unsigned int position) {
+	return false;
+}
+
+bool List::Remove(unsigned int position) {
+	return false;
+}
+
+int List::IndexOf(const Book &book) const {
+	return 0;
+}
+
+Book *List::Get(unsigned int position) const {
+	return nullptr;
+}
+
+void List::Write(ostream &output) const {
+
+}
+
+size_t List::GetSize() const {
+	return 0;
+}
