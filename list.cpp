866c2e1900dd91dfe2245776ab581c71f4322f3b
Carlos R. Arias <arias@spu.edu>
2020-11-13 17:09:22 -0800

    Insert

--- list.cpp
+++ list.cpp
@@ -22,7 +22,26 @@ List::~List() {
 }
 
 bool List::Insert(const Book &book, unsigned int position) {
-	return false;
+	if (position > _size)
+		return false;
+	Node* neo = new Node;
+	neo->book = new Book(book);
+	neo->next = nullptr;
+	if (_size == 0){ // Do we need to check position == 0?
+		_head = neo;
+	}else if (position == 0){ // Why is this correct?
+		neo->next = _head;
+		_head = neo;
+	}else{ // Why don't we need a "insert at last" case? Does this one work for that case?
+		Node* tmp = _head;
+		for (unsigned int i=0; i < position - 1; i++){ // Are we sure position - 1 cannot be negative?
+			tmp = tmp->next;
+		}
+		neo->next = tmp->next;
+		tmp->next = neo;
+	}
+	_size++;
+	return true;
 }
 
 bool List::Remove(unsigned int position) {
